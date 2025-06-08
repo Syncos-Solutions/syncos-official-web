f36d5b3530abbdcf9d6460a47c20614f29667ca9
Syncos Solutions <195300108+Syncos-Solutions@users.noreply.github.com>
2025-06-08 20:35:05 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 // © 2025 gihantha deshabi. All Rights Reserved. 2025-03-28
 
 import Features from "@/components/Features";
-import Getapp from "@/components/Getapp";
+
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 
@@ -14,7 +14,7 @@ export default function Home() {
     <Services />
     
     
-    <Getapp />
+   
     </>
   );
 }
